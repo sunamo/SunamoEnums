b909149202a9e8dd6859d73f5485745395c45155
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 11:32:09 +0200

    Make some classes public when it is needed

--- Enums/AppFolders.cs
+++ Enums/AppFolders.cs
@@ -1,23 +1,23 @@
 
-namespace SunamoEnums;
-public enum AppFolders
-{
-    #region Not backuped
-    Logs,
-    Output,
-    Cache,
-    Temp,
-    #endregion
-    #region Backuped
-    Input,
-    Settings,
-    Data,
-    Other,
-    Controls,
-    Local,
-    Roaming,
-    Crypted,
-    Reports,
-    Backup
-    #endregion
-}
+//namespace SunamoEnums;
+//public enum AppFolders
+//{
+//    #region Not backuped
+//    Logs,
+//    Output,
+//    Cache,
+//    Temp,
+//    #endregion
+//    #region Backuped
+//    Input,
+//    Settings,
+//    Data,
+//    Other,
+//    Controls,
+//    Local,
+//    Roaming,
+//    Crypted,
+//    Reports,
+//    Backup
+//    #endregion
+//}
